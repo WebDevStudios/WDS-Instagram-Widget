007d83ae074b761227ac0b77b0ca6b8211b04607
Patrick Garman <patrick@pmgarman.me>
2014-07-31 13:22:19 -0500

    Let there be instagrams

--- instagram-widget.php
+++ instagram-widget.php
@@ -22,8 +22,8 @@ class WDS_Instagram_Widget extends WP_Widget {
 
 		parent::__construct(
 			'wds_instagram_widget', // Base ID
-			__( 'Instagram Widget', 'textdomain' ), // Name
-			array( 'description' => __( 'Display your latest Instagrams in a sidebar widget.', 'textdomain' ) ) // Args
+			__( 'Instagram Widget', 'wds-instagram' ), // Name
+			array( 'description' => __( 'Display your latest Instagrams in a sidebar widget.', 'wds-instagram' ) ) // Args
 		);
 
 	}
@@ -44,33 +44,46 @@ class WDS_Instagram_Widget extends WP_Widget {
 		$username = ( ! empty( $instance['username'] ) ) ? esc_attr( $instance['username'] ) : '';
 
 		// Get Instagrams
-		$instagram = maybe_unserialize( $this->get_instagrams( array(
-				'user_id'   => $instance['user_id'],
-				'client_id' => $instance['client_id'],
-			)
+		$instagram = $this->get_instagrams( array(
+			'user_id'   => $instance['user_id'],
+			'client_id' => $instance['client_id'],
+			'count'     => $instance['count'],
 		) );
 
+		if( false !== $instagram ): ?>
 
-		echo $args['before_widget'];
+		<?php
+			// Allow the image resolution to be filtered to use any available image resolutions from Instagram
+			// low_resolution, thumbnail, standard_resolution
+			$image_res = apply_filters( 'wds_instagram_widget_image_resolution', 'thumbnail' );
+
+			echo $args['before_widget'];
+			echo $args['before_title'] . esc_html( $title ) . $args['after_title'];
 		?>
 
-		<div class="instagram-widget">
-			<?php echo $args['before_title'] . esc_html( $title ) . $args['after_title'];
+		<ul class="instagram-widget">
 
-			wp_die( '<pre>'. htmlentities( print_r( $instagram, true ) ) .'</pre>' );
-
-			foreach ($instagram['data'] as $key => $value) {
-				wp_die( '<pre>'. htmlentities( print_r( $instagram, true ) ) .'</pre>' );
-			}
-
-
-			?>
-
-			<a href="https://instagram.com/<?php echo esc_html( $username ); ?>"><?php echo esc_html( $username ); ?></a>
-		</div>
 		<?php
+			foreach( $instagram['data'] as $key => $image ) {
+				echo apply_filters( 'wds_instagram_widget_image_html', sprintf( '<li class="instagram-image"><a href="%1$s"><img src="%2$s" /></a></li>',
+					$image['link'],
+					$image['images'][ $image_res ]['url']
+				), $image );
+			}
+		?>
 
-		echo $args['after_widget'];
+			<a href="https://instagram.com/<?php echo esc_html( $username ); ?>"><?php printf( __( 'Follow %1$s on Instagram!', 'wds-instagram' ), esc_html( $username ) ); ?></a>
+		</ul>
+
+		<?php
+			echo $args['after_widget'];
+		?>
+
+		<?php elseif( ( defined( 'WP_DEBUG' ) && true === WP_DEBUG ) && ( defined( 'WP_DEBUG_DISPLAY' ) && false !== WP_DEBUG_DISPLAY ) ): ?>
+			<div id="message" class="error"><p><?php _e( 'Error: We were unable to fetch your instagram feed.', 'wds-instagram' ); ?></p></div>
+		<?php endif;
+
+
 	}
 
 
@@ -93,7 +106,7 @@ class WDS_Instagram_Widget extends WP_Widget {
 
 		$this->form_input(
 			array(
-				'label'       => __( 'Widget Title:', 'textdomain'),
+				'label'       => __( 'Widget Title:', 'wds-instagram'),
 				'name'        => $this->get_field_name( 'title' ),
 				'id'          => $this->get_field_id( 'title' ),
 				'type'        => 'text',
@@ -104,7 +117,7 @@ class WDS_Instagram_Widget extends WP_Widget {
 
 		$this->form_input(
 			array(
-				'label'       => __( 'Username:', 'textdomain'),
+				'label'       => __( 'Username:', 'wds-instagram'),
 				'name'        => $this->get_field_name( 'username' ),
 				'id'          => $this->get_field_id( 'username' ),
 				'type'        => 'text',
@@ -115,7 +128,7 @@ class WDS_Instagram_Widget extends WP_Widget {
 
 		$this->form_input(
 			array(
-				'label'       => __( 'User ID:', 'textdomain'),
+				'label'       => __( 'User ID:', 'wds-instagram'),
 				'name'        => $this->get_field_name( 'user_id' ),
 				'id'          => $this->get_field_id( 'user_id' ),
 				'type'        => 'text',
@@ -126,18 +139,19 @@ class WDS_Instagram_Widget extends WP_Widget {
 
 		$this->form_input(
 			array(
-				'label'       => __( 'Client ID:', 'textdomain'),
+				'label'       => __( 'Client ID:', 'wds-instagram'),
 				'name'        => $this->get_field_name( 'client_id' ),
 				'id'          => $this->get_field_id( 'client_id' ),
 				'type'        => 'text',
 				'value'       => $client_id,
-				'placeholder' => '943c899bab2a47e6ae341d3d1943e73f'
+				'placeholder' => '943c899bab2a47e6ae341d3d1943e73f',
+				'desc'        => sprintf( __( 'Register a new client <a href="%1$s" target="_blank">here</a>', 'wds-instagram' ), 'http://instagram.com/developer/clients/manage/' )
 			)
 		);
 
 		$this->form_input(
 			array(
-				'label'       => __( 'Photo Count:', 'textdomain'),
+				'label'       => __( 'Photo Count:', 'wds-instagram'),
 				'name'        => $this->get_field_name( 'count' ),
 				'id'          => $this->get_field_id( 'count' ),
 				'type'        => 'text',
@@ -167,7 +181,7 @@ class WDS_Instagram_Widget extends WP_Widget {
 				$instance[$value] = sanitize_text_field( $new_instance[$value] );
 			}
 
-			delete_transient( 'wds_instagram_widget_' . $this->id );
+			delete_transient( 'wds_instagram_widget_' . md5( $this->id, $instance['count'] ) );
 
 		return $instance;
 
@@ -181,22 +195,33 @@ class WDS_Instagram_Widget extends WP_Widget {
 	 */
 	public function form_input( $args = array() ) {
 
+		$args = wp_parse_args( $args, array(
+			'label'       => '',
+			'name'        => '',
+			'id'          => '',
+			'type'        => 'text',
+			'value'       => '',
+			'placeholder' => '',
+			'desc'        => ''
+		) );
+
 		$label       = esc_html( $args['label'] );
 		$name        = esc_html( $args['name'] );
 		$id          = esc_html( $args['id'] );
 		$type        = esc_html( $args['type'] );
 		$value       = esc_html( $args['value'] );
 		$placeholder = esc_html( $args['placeholder'] );
+		$desc        = ! empty( $args['desc'] ) ? sprintf( '<span class="description">%1$s</span>', $args['desc'] ) : '';
 
 		printf(
-			'<p><label for="%s">%s</label><input type="%s" class="widefat" name="%s" id="%s" value="%s" placeholder="%s" /></p>',
+			'<p><label for="%1$s">%2$s</label><input type="%3$s" class="widefat" name="%4$s" id="%1$s" value="%5$s" placeholder="%6$s" />%7$s</p>',
 			$id,
 			$label,
 			$type,
 			$name,
-			$id,
 			$value,
-			$placeholder
+			$placeholder,
+			$desc
 		);
 	}
 
@@ -212,33 +237,41 @@ class WDS_Instagram_Widget extends WP_Widget {
 		// Get args
 		$user_id   = ( ! empty( $args['user_id'] ) ) ? $args['user_id'] : '';
 		$client_id = ( ! empty( $args['client_id'] ) ) ? $args['client_id'] : '';
+		$count     = ( ! empty( $args['count'] ) ) ? $args['count'] : '';
 
 		// If no client id or user id, bail
 		if ( empty( $client_id ) || empty( $user_id ) ) {
-			return $instagrams;
+			return false;
 		}
 
 		// Check for transient
-		if ( false === ( $instagrams = get_transient( 'wds_instagram_widget_' . $this->id ) ) ) {
+		$key = 'wds_instagram_widget_' . md5( $this->id, $args['count'] );
+		if ( false === ( $instagrams = get_transient( $key ) ) ) {
 
 			// Ping Instragram's API
-			$response = wp_remote_get( 'https://api.instagram.com/v1/users/' . esc_html( $user_id ) . '/media/recent/?client_id=' . esc_html( $client_id ) );
+			$api_url = 'https://api.instagram.com/v1/users/' . esc_html( $user_id ) . '/media/recent/';
+			$response = wp_remote_get( add_query_arg( array(
+				'client_id' => esc_html( $client_id ),
+				'count'     => absint( $count )
+			), $api_url ) );
 
-				// Is the API up?
-				if ( ! 200 == wp_remote_retrieve_response_code( $response ) ) {
-					return '<div id="message" class="error"><p>' . __( 'You\'ve entered invalid credentials or the Instagram API down.', 'textdomain' ) . '</p></div>';
-				}
+			// Is the API up?
+			if ( ! 200 == wp_remote_retrieve_response_code( $response ) ) {
+				return false;
+			}
 
 			// Parse the API data and place into an array
 			$instagrams = json_decode( wp_remote_retrieve_body( $response ), true );
 
-				// Are the results in an array?
-				if ( ! is_array( $instagrams ) ) {
-					return '<div id="message" class="error"><p>' . __( 'The results from the Instagram API were invalid. Please try again later.', 'textdomain' ) . '</p></div>';
-				}
+			// Are the results in an array?
+			if ( ! is_array( $instagrams ) ) {
+				return false;
+			}
+
+			$instagrams = maybe_unserialize( $instagrams );
 
 			// Store Instagrams in a transient, and expire every hour
-			set_transient( 'wds_instagram_widget_' . $this->id, $instagrams, 60 * MINUTE_IN_SECONDS );
+			set_transient( $key, $instagrams, apply_filters( 'wds_instagram_widget_cache_lifetime', 60 * MINUTE_IN_SECONDS ) );
 		}
 
 		return $instagrams;
